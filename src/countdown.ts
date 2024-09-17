17cd4a793c191551774aca00d135dc90b5a76a90
iKarTehFox <dperez81715+github@gmail.com>
2024-09-17 17:04:14 -0500

    chore: refactor and streamline stopwatch/countdown functions

--- src/countdown.ts
+++ src/countdown.ts
@@ -16,20 +16,14 @@ function updateDisplay() {
         .join(':');
 }
 
-function inputsDisabled(disabled: boolean) {
-    if (disabled === false) {
-        countdown.hrsinput.disabled = false;
-        countdown.mininput.disabled = false;
-        countdown.secinput.disabled = false;
-    } else {
-        countdown.hrsinput.disabled = true;
-        countdown.mininput.disabled = true;
-        countdown.secinput.disabled = true;
-    }
+function inputsState(disabled: boolean) {
+    countdown.hrsinput.disabled = disabled;
+    countdown.mininput.disabled = disabled;
+    countdown.secinput.disabled = disabled;
 }
 
-function disableBtns(buttons: string[], disabled: boolean) {
-    buttons.forEach(button => {
+function btnState(buttonStates: { [key: string]: boolean }) {
+    Object.entries(buttonStates).forEach(([button, disabled]) => {
         match(button)
             .with('start', () => {
                 countdown.startbtn.disabled = disabled;
@@ -59,14 +53,20 @@ function startCountdown() {
                 clearInterval(countdownInterval);
                 running = false;
                 showToast('Countdown finished!', 30000, 'success');
-                inputsDisabled(false);
-                disableBtns(['start'], false);
-                disableBtns(['pause', 'reset'], true);
+                inputsState(false);
+                btnState({
+                    start: false,
+                    pause: true,
+                    reset: true,
+                });
             }
         }, 1000);
         logConsole('Countdown started...', 'info');
-        disableBtns(['start'], true);
-        disableBtns(['pause', 'reset'], false);
+        btnState({
+            start: true,
+            pause: false,
+            reset: false,
+        });
     }
 }
 
@@ -75,8 +75,10 @@ function pauseCountdown() {
         running = false;
         clearInterval(countdownInterval);
         logConsole('Countdown paused...', 'info');
-        disableBtns(['start'], false);
-        disableBtns(['pause'], true);
+        btnState({
+            start: false,
+            pause: true,
+        });
     }
 }
 
@@ -88,10 +90,13 @@ function resetCountdown() {
         updateDisplay();
     
         // Re-enable inputs
-        inputsDisabled(false);
+        inputsState(false);
         logConsole('Countdown reset...', 'info');
-        disableBtns(['start'], false);
-        disableBtns(['pause', 'reset'], true);
+        btnState({
+            start: false,
+            pause: true,
+            reset: true,
+        });
     }
 }
 
@@ -116,7 +121,7 @@ countdown.startbtn.addEventListener('click', () => {
         startCountdown();
 
         // Disable inputs
-        inputsDisabled(true);
+        inputsState(true);
     }
 });
 
--- src/stopwatch.ts
+++ src/stopwatch.ts
@@ -36,8 +36,8 @@ function updateDisplay(returnCurrent: boolean = false, isFormatted: boolean = fa
     stopwatch.display.textContent = formatTime(timeDiff);
 }
 
-function disableBtns(buttons: string[], disabled: boolean) {
-    buttons.forEach(button => {
+function btnState(buttons: { [key: string]: boolean }) {
+    Object.entries(buttons).forEach(([button, disabled]) => {
         match(button)
             .with('start', () => {
                 stopwatch.startbtn.disabled = disabled;
@@ -64,9 +64,12 @@ function startStopwatch() {
         startTime = Date.now();
         timeInterval = setInterval(updateDisplay, 25);
         logConsole('Stopwatch started...', 'info');
-        disableBtns(['start'], true);
-        disableBtns(['pause', 'reset'], false);
-        disableBtns(['lap'], false);
+        btnState({
+            start: true,
+            pause: false,
+            reset: false,
+            lap: false,
+        });
     }
 }
 
@@ -77,9 +80,11 @@ export function pauseStopwatch() {
         elapsedTime += Date.now() - startTime;
         clearInterval(timeInterval);
         logConsole('Stopwatch paused...', 'info');
-        disableBtns(['start'], false);
-        disableBtns(['pause'], true);
-        disableBtns(['lap'], true);
+        btnState({
+            start: false,
+            pause: true,
+            lap: true,
+        });
     }
 }
 
@@ -91,9 +96,12 @@ function resetStopwatch() {
         elapsedTime = 0;
         updateDisplay();
         logConsole('Stopwatch reset...', 'info');
-        disableBtns(['start'], false);
-        disableBtns(['pause', 'reset'], true);
-        disableBtns(['lap'], false);
+        btnState({
+            start: false,
+            pause: true,
+            reset: true,
+            lap: false,
+        });
 
         // Clear lap textarea history
         stopwatch.lapfield.value ='';
