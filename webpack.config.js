711903d5105b8cf1f75984893f227dcdeb807cad
iKarTehFox <dperez81715+github@gmail.com>
2024-12-01 20:25:24 -0600

    fix: set hot to false for liveReload

--- webpack.config.js
+++ webpack.config.js
@@ -46,9 +46,15 @@ const config = {
         })
     ],
     devServer: {
-        open: false,
-        host: 'localhost',
-    },
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        hot: false,
+        liveReload: true,
+        compress: true,
+        host: '127.0.0.1',
+        port: 4000,
+      },
     module: {
         rules: [
             {
