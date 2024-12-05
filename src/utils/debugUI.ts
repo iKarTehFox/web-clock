d56256a69a9ffe5805145ab717c7bbbda19bcd5a
iKarTehFox <dperez81715+github@gmail.com>
2024-12-05 01:15:44 -0600

    chore: move debug options to own section

--- src/utils/debugUI.ts
+++ src/utils/debugUI.ts
@@ -4,7 +4,7 @@ import { debugMode } from './debug';
 export function initializeDebugUI(): void {
     if (debugMode) {
         // Enable debug container
-        debug.container.style.display = 'block';
+        debug.container.style.display = '';
 
         // Fill debug info
         debug.uastring.textContent = navigator.userAgent;
