0673b0ed0dc6c97afabe05690d22c33cc1d7cb39
iKarTehFox <dperez81715+github@gmail.com>
2024-12-05 01:12:26 -0600

    chore: change webpack devserver to listen on LAN

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ const config = {
         hot: false,
         liveReload: true,
         compress: true,
-        host: '127.0.0.1',
+        host: '0.0.0.0',
         port: 4000,
       },
     module: {
