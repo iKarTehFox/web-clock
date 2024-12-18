fc5b7cecf701e6c5af8339c54f1710e6942370d8
iKarTehFox <dperez81715+github@gmail.com>
2024-12-17 21:45:21 -0600

    fix: actually fix first lap invalid last lap time
    Didn't check before making the fix, huh?

--- src/stopwatch.ts
+++ src/stopwatch.ts
@@ -7,7 +7,7 @@ let running: boolean = false;
 let startTime: number;
 let elapsedTime: number = 0;
 let counter: number = 0;
-let lastLapTime = 0;
+let lastLapTime;
 
 // Format time to HH:MM:SS.mmm
 function formatTime(totalMilliseconds: number) {
@@ -103,9 +103,10 @@ function resetStopwatch() {
             lap: false,
         });
 
-        // Clear lap textarea history
+        // Reset display
         stopwatch.lapfield.value ='';
         counter = 0;
+        lastLapTime = undefined;
     }
 }
 
