17e571c8ccb86eee53fa39ef1a0451037f36c6a9
iKarTehFox <dperez81715+github@gmail.com>
2025-04-07 17:21:04 -0500

    chore: set export version

--- src/importValidation.ts
+++ src/importValidation.ts
@@ -23,7 +23,7 @@ const valid = {
     TCM: ['tcovD', 'tcovO'],
     BIS: ['', 'auto', 'cover', 'stretch'],
     BIB: ['', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-    Ver: [10]
+    Ver: [11]
 };
 
 // Error handling
