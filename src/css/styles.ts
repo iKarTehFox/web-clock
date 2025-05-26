15a5c6fd5a154515584e59b348849c88a00dcc87
iKarTehFox <dperez81715+github@gmail.com>
2025-05-26 01:14:58 -0500

    chore: transition toastify.css to SCSS

--- src/css/styles.ts
+++ src/css/styles.ts
@@ -3,5 +3,5 @@ import './global.scss';
 import './fonts.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap-social.css';
-import './toastify.css';
+import './toastify.scss';
 import './themes/midnight.css';
