6b4ce9f4ef4e87b9f1549a8235a2052fdc0b0b85
iKarTehFox <dperez81715+github@gmail.com>
2025-05-30 06:21:40 -0500

    chore: transition midnight.css to SCSS
    Also lock sass version to older release to avoid deprecation warnings.

--- src/css/styles.ts
+++ src/css/styles.ts
@@ -1,7 +1,6 @@
 // CSS Imports
 import './global.scss';
 import './fonts.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap-social.css';
 import './toastify.scss';
-import './themes/midnight.css';
+import './themes/midnight.scss';
