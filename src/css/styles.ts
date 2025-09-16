d6fe24f2834bf73a305a9c28704c88d5971fc3fc
iKarTehFox <dperez81715+github@gmail.com>
2025-09-16 16:36:46 -0500

    chore: cleanup unused bootstrap-social

--- src/css/styles.ts
+++ src/css/styles.ts
@@ -1,7 +1,6 @@
 // CSS Imports
 import './global.scss';
 import './fonts.css';
-import './bootstrap-social.css';
 import './bs-icons.scss';
 import './themes/midnight.scss';
 import './themes/amoled.scss';
